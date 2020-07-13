35552b6804189ffff35317ad0129f30832b074a0
luzcry <mcrodriguezt618@gmail.com>
2020-07-12 23:37:15 -0500

    vue router y router-link

---
+++ vue-exchange/src/views/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Platzi exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyecto para obtener cotizaciones de las cryptomonedas mas importantes a traves de API REST de coincap
+      <br />Este proyecto es utilizado en el curso de vue.js basico de platzi
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline">
+        Volver a la página de inicio
+    </router-link>
+  </div>
+</template>
