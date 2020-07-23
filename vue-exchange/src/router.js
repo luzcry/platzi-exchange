8a570f983e1ff0ea40e8c0f3893f0ccfab57b653
luzcry <mcrodriguezt618@gmail.com>
2020-07-23 00:24:08 -0500

    rutas dinamicas vista precios incompleto

--- vue-exchange/src/router.js
+++ vue-exchange/src/router.js
@@ -3,7 +3,7 @@ import Router from "vue-router";
 import Home from "@/views/Home";
 import Error from "@/views/Error";
 import About from "@/views/about";
-
+import CoinDetail from "@/views/CoinDetail";
 Vue.use(Router);
 
 export default new Router({
@@ -20,7 +20,11 @@ export default new Router({
       name: "about",
       component: About
     },
-
+    {
+      path: "/coin/:id",
+      name: "coin-detail",
+      component: CoinDetail
+    },
     {
       path: "*",
       name: "Error",
